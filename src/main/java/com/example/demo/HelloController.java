83c27dbc497543b4410f991fd5ab4d9093ce9996
jifengdehao <2975830779@qq.com>
2017-07-24 15:41:13 +0800

    15：41

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${cupSize}")
+    private String cupSize;
+
+    @Value("${age}")
+    private Integer age;
+
+    @Value("${content}")
+    private String content;
+
+    @Autowired
+    private GirlProperties girlProperties;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String say() {
-        return "hello Spring Boot";
+        //return "hello Spring Boot";
+        //return cupSize +"--"+ age;
+        // return content;
+        return girlProperties.getCupSize();
     }
 }
